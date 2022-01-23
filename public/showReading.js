e7410ee888c86e005a5dd51e9e4d2b7544b008d5
Jacob D. Stephens <jacobdstephens@gmail.com>
2022-01-23 13:00:48 -0800

    Adding showReading function

--- public/showReading.js
+++ public/showReading.js
@@ -1,1 +1,10 @@
-
+function showReading() 
+{
+  var x = document.getElementById("myDIV");
+  if (x.style.display === "none") 
+  {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
